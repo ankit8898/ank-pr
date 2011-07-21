022de0efa189858b1bf34bbc670ca6fd4452dbe2
ankit gupta <ankit.gupta8898@gmail.com>
2011-07-21 11:19:28 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnkPr::Application.config.secret_token = 'c3001156927ce823b39d3a669f04b0fffd098a0f04f7f35097a53b983a2f160190f50ab383b1f7e6874d943da8ac43f0c824f298a34c88c812f996c3d6db0f22'
